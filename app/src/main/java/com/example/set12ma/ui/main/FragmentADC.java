ee22330cc2417f972920486c40de1b57d811d639
sokolovstanislav <ignateva-1946@mail.ru>
2022-08-25 14:56:36 +0300

    Added switches to FragmentADC.

--- app/src/main/java/com/example/set12ma/ui/main/FragmentADC.java
+++ app/src/main/java/com/example/set12ma/ui/main/FragmentADC.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -29,6 +30,7 @@ public class FragmentADC extends Fragment {
 
     private ArrayList<Button> arrayListButton;
     private ArrayList<TextView> arrayListTextView;
+    private ArrayList<Switch> arrayListSwitch;
     private SpaceAddress spaceAddress;
     private ResultReceiverAddressSpace resultReceiverAddressSpace;
     private SpaceStatus spaceStatus;
@@ -42,6 +44,59 @@ public class FragmentADC extends Fragment {
     private ArrayList<Chart> arrayListChart;
 
     private int time = 0;
+
+    private Switch switch_adc_0_0;
+    private Switch switch_adc_0_1;
+    private Switch switch_adc_0_2;
+    private Switch switch_adc_0_3;
+    private Switch switch_adc_0_4;
+    private Switch switch_adc_0_5;
+    private Switch switch_adc_0_6;
+    private Switch switch_adc_0_7;
+    private Switch switch_adc_0_8;
+    private Switch switch_adc_0_9;
+    private Switch switch_adc_0_10;
+    private Switch switch_adc_0_11;
+    private Switch switch_adc_0_12;
+    private Switch switch_adc_0_13;
+    private Switch switch_adc_0_14;
+    private Switch switch_adc_0_15;
+
+    private Switch switch_adc_1_0;
+    private Switch switch_adc_1_1;
+    private Switch switch_adc_1_2;
+    private Switch switch_adc_1_3;
+    private Switch switch_adc_1_4;
+    private Switch switch_adc_1_5;
+    private Switch switch_adc_1_6;
+    private Switch switch_adc_1_7;
+    private Switch switch_adc_1_8;
+    private Switch switch_adc_1_9;
+    private Switch switch_adc_1_10;
+    private Switch switch_adc_1_11;
+    private Switch switch_adc_1_12;
+    private Switch switch_adc_1_13;
+    private Switch switch_adc_1_14;
+    private Switch switch_adc_1_15;
+
+    private Switch switch_adc_2_0;
+    private Switch switch_adc_2_1;
+    private Switch switch_adc_2_2;
+    private Switch switch_adc_2_3;
+    private Switch switch_adc_2_4;
+    private Switch switch_adc_2_5;
+    private Switch switch_adc_2_6;
+    private Switch switch_adc_2_7;
+    private Switch switch_adc_2_8;
+    private Switch switch_adc_2_9;
+    private Switch switch_adc_2_10;
+    private Switch switch_adc_2_11;
+    private Switch switch_adc_2_12;
+    private Switch switch_adc_2_13;
+    private Switch switch_adc_2_14;
+    private Switch switch_adc_2_15;
+
+
 
     @Override
     public void onAttach(Context context) {
@@ -101,6 +156,7 @@ public class FragmentADC extends Fragment {
         });
         arrayListTextView = new ArrayList<>();
         arrayListButton = new ArrayList<>();
+        arrayListSwitch = new ArrayList<>();
 
         TextView textViewValue_adc_0_0 = root.findViewById(R.id.textViewValue_adc_0_0);
         arrayListTextView.add(textViewValue_adc_0_0);
@@ -300,6 +356,393 @@ public class FragmentADC extends Fragment {
         Button indicator_button_adc_2_15 = root.findViewById(R.id.indicator_button_adc_2_15);
         arrayListButton.add(indicator_button_adc_2_15);
 
+        switch_adc_0_0 = root.findViewById(R.id.switch_adc_0_0);
+        switch_adc_0_0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(0, 0, switch_adc_0_0.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_0_0);
+        switch_adc_0_1 = root.findViewById(R.id.switch_adc_0_1);
+        switch_adc_0_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(0, 1, switch_adc_0_1.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_0_1);
+        switch_adc_0_2 = root.findViewById(R.id.switch_adc_0_2);
+        switch_adc_0_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(0, 2, switch_adc_0_2.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_0_2);
+        switch_adc_0_3 = root.findViewById(R.id.switch_adc_0_3);
+        switch_adc_0_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(0, 3, switch_adc_0_3.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_0_3);
+        switch_adc_0_4 = root.findViewById(R.id.switch_adc_0_4);
+        switch_adc_0_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(0, 4, switch_adc_0_4.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_0_4);
+        switch_adc_0_5 = root.findViewById(R.id.switch_adc_0_5);
+        switch_adc_0_5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(0, 5, switch_adc_0_5.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_0_5);
+        switch_adc_0_6 = root.findViewById(R.id.switch_adc_0_6);
+        switch_adc_0_6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(0, 6, switch_adc_0_6.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_0_6);
+        switch_adc_0_7 = root.findViewById(R.id.switch_adc_0_7);
+        switch_adc_0_7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(0, 7, switch_adc_0_7.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_0_7);
+        switch_adc_0_8 = root.findViewById(R.id.switch_adc_0_8);
+        switch_adc_0_8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(0, 8, switch_adc_0_8.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_0_8);
+        switch_adc_0_9 = root.findViewById(R.id.switch_adc_0_9);
+        switch_adc_0_9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(0, 9, switch_adc_0_9.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_0_9);
+        switch_adc_0_10 = root.findViewById(R.id.switch_adc_0_10);
+        switch_adc_0_10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(0, 10, switch_adc_0_10.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_0_10);
+        switch_adc_0_11 = root.findViewById(R.id.switch_adc_0_11);
+        switch_adc_0_11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(0, 11, switch_adc_0_11.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_0_11);
+        switch_adc_0_12 = root.findViewById(R.id.switch_adc_0_12);
+        switch_adc_0_12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(0, 12, switch_adc_0_12.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_0_12);
+        switch_adc_0_13 = root.findViewById(R.id.switch_adc_0_13);
+        switch_adc_0_13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(0, 13, switch_adc_0_13.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_0_13);
+        switch_adc_0_14 = root.findViewById(R.id.switch_adc_0_14);
+        switch_adc_0_14.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(0, 14, switch_adc_0_14.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_0_14);
+        switch_adc_0_15 = root.findViewById(R.id.switch_adc_0_15);
+        switch_adc_0_15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(0, 15, switch_adc_0_15.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_0_15);
+
+        switch_adc_1_0 = root.findViewById(R.id.switch_adc_1_0);
+        switch_adc_1_0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(1, 0, switch_adc_1_0.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_1_0);
+        switch_adc_1_1 = root.findViewById(R.id.switch_adc_1_1);
+        switch_adc_1_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(1, 1, switch_adc_1_1.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_1_1);
+        switch_adc_1_2 = root.findViewById(R.id.switch_adc_1_2);
+        switch_adc_1_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(1, 2, switch_adc_1_2.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_1_2);
+        switch_adc_1_3 = root.findViewById(R.id.switch_adc_1_3);
+        switch_adc_1_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(1, 3, switch_adc_1_3.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_1_3);
+        switch_adc_1_4 = root.findViewById(R.id.switch_adc_1_4);
+        switch_adc_1_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(1, 4, switch_adc_1_4.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_1_4);
+        switch_adc_1_5 = root.findViewById(R.id.switch_adc_1_5);
+        switch_adc_1_5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(1, 5, switch_adc_1_5.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_1_5);
+        switch_adc_1_6 = root.findViewById(R.id.switch_adc_1_6);
+        switch_adc_1_6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(1, 6, switch_adc_1_6.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_1_6);
+        switch_adc_1_7 = root.findViewById(R.id.switch_adc_1_7);
+        switch_adc_1_7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(1, 7, switch_adc_1_7.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_1_7);
+        switch_adc_1_8 = root.findViewById(R.id.switch_adc_1_8);
+        switch_adc_1_8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(1, 8, switch_adc_1_8.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_1_8);
+        switch_adc_1_9 = root.findViewById(R.id.switch_adc_1_9);
+        switch_adc_1_9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(1, 9, switch_adc_1_9.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_1_9);
+        switch_adc_1_10 = root.findViewById(R.id.switch_adc_1_10);
+        switch_adc_1_10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(1, 10, switch_adc_1_10.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_1_10);
+        switch_adc_1_11 = root.findViewById(R.id.switch_adc_1_11);
+        switch_adc_1_11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(1, 11, switch_adc_1_11.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_1_11);
+        switch_adc_1_12 = root.findViewById(R.id.switch_adc_1_12);
+        switch_adc_1_12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(1, 12, switch_adc_1_12.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_1_12);
+        switch_adc_1_13 = root.findViewById(R.id.switch_adc_1_13);
+        switch_adc_1_13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(1, 13, switch_adc_1_13.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_1_13);
+        switch_adc_1_14 = root.findViewById(R.id.switch_adc_1_14);
+        switch_adc_1_14.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(1, 14, switch_adc_1_14.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_1_14);
+        switch_adc_1_15 = root.findViewById(R.id.switch_adc_1_15);
+        switch_adc_1_15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(1, 15, switch_adc_1_15.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_1_15);
+
+        switch_adc_2_0 = root.findViewById(R.id.switch_adc_2_0);
+        switch_adc_2_0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(2, 0, switch_adc_2_0.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_2_0);
+        switch_adc_2_1 = root.findViewById(R.id.switch_adc_2_1);
+        switch_adc_2_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(2, 1, switch_adc_2_1.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_2_1);
+        switch_adc_2_2 = root.findViewById(R.id.switch_adc_2_2);
+        switch_adc_2_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(2, 2, switch_adc_2_2.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_2_2);
+        switch_adc_2_3 = root.findViewById(R.id.switch_adc_2_3);
+        switch_adc_2_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(2, 3, switch_adc_2_3.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_2_3);
+        switch_adc_2_4 = root.findViewById(R.id.switch_adc_2_4);
+        switch_adc_2_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(2, 4, switch_adc_2_4.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_2_4);
+        switch_adc_2_5 = root.findViewById(R.id.switch_adc_2_5);
+        switch_adc_2_5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(2, 5, switch_adc_2_5.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_2_5);
+        switch_adc_2_6 = root.findViewById(R.id.switch_adc_2_6);
+        switch_adc_2_6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(2, 6, switch_adc_2_6.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_2_6);
+        switch_adc_2_7 = root.findViewById(R.id.switch_adc_2_7);
+        switch_adc_2_7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(2, 7, switch_adc_2_7.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_2_7);
+        switch_adc_2_8 = root.findViewById(R.id.switch_adc_2_8);
+        switch_adc_2_8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(2, 8, switch_adc_2_8.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_2_8);
+        switch_adc_2_9 = root.findViewById(R.id.switch_adc_2_9);
+        switch_adc_2_9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(2, 9, switch_adc_2_9.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_2_9);
+        switch_adc_2_10 = root.findViewById(R.id.switch_adc_2_10);
+        switch_adc_2_10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(2, 10, switch_adc_2_10.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_2_10);
+        switch_adc_2_11 = root.findViewById(R.id.switch_adc_2_11);
+        switch_adc_2_11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(2, 11, switch_adc_2_11.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_2_11);
+        switch_adc_2_12 = root.findViewById(R.id.switch_adc_2_12);
+        switch_adc_2_12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(2, 12, switch_adc_2_12.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_2_12);
+        switch_adc_2_13 = root.findViewById(R.id.switch_adc_2_13);
+        switch_adc_2_13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(2, 13, switch_adc_2_13.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_2_13);
+        switch_adc_2_14 = root.findViewById(R.id.switch_adc_2_14);
+        switch_adc_2_14.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(2, 14, switch_adc_2_14.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_2_14);
+        switch_adc_2_15 = root.findViewById(R.id.switch_adc_2_15);
+        switch_adc_2_15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show(2, 15, switch_adc_2_15.isChecked());
+            }
+        });
+        arrayListSwitch.add(switch_adc_2_15);
+
         upDateValues();
 
         upDateGraphicalDisplay = new UpDateGraphicalDisplay();
@@ -342,6 +785,10 @@ public class FragmentADC extends Fragment {
         LineData data = new LineData(dataSets);
         lineChart.setData(data);
         lineChart.invalidate();
+    }
+
+    public void show(int adc, int channel, boolean b) {
+        arrayListChart.get(adc).getArrayList().get(channel).setEnableShow(b);
     }
 
     public class UpDateGraphicalDisplay extends Thread {
